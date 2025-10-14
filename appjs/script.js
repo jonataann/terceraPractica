7c97d91e6ae134fc860252c0314e2befc4c18315
Jorge Jose <jorge_antezana@usmp.pe>
2025-10-14 12:49:21 -0500

    proyecto

---
+++ appjs/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#ocultar").click(function(){
+        $("#parrafo2").hide();
+    });
+    $("#mostrar").click(function(){
+        $("#parrafo2").show();
+    });
+});
